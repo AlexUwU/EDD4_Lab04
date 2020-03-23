989b3045b9c913af95508b96271524e15430c90c
AlexUwU <alexander.2014022@gmail.com>
2020-03-22 18:41:57 -0600

    terminado

---
+++ nombreFacil/nombreFacil/Controllers/WeatherForecastController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using nombreFacil.Models;
+
+namespace nombreFacil.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+
+        public WeatherForecastController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+
+        }
+        public class FileUploadAPI
+        {
+            public IFormFile files { get; set; }
+
+        }
+        
+
+
+        [HttpPost("Compress")]
+        public async Task<string> post([FromForm]FileUploadAPI objFile)
+        {
+            
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        fileStream.Close();
+                        string s = @_environment.WebRootPath;
+                        Implementacion imp = new Implementacion(fileStream.Name, s);
+                        imp.Comprimir();
+                        
+                        return "\\Upload\\" + objFile.files.FileName;
+
+                    }
+                    
+
+                }
+                else
+                {
+                    return "Failed";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+
+        
+
+        }
+       
+        [HttpPost("decompress")]
+        
+        public async Task<string> decompress([FromForm]FileUploadAPI objFile)
+        {
+
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        fileStream.Close();
+                        string s = @_environment.WebRootPath;
+                        Implementacion imp = new Implementacion(fileStream.Name, s);
+                        imp.Descomporimir();
+
+                        return "\\Upload\\" + objFile.files.FileName;
+
+                    }
+
+
+                }
+                else
+                {
+                    return "Failed";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+
+
+
+        }
+
+    }
+}
